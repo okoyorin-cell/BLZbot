4822ca9c42f519d9e9af0f223599de32f431aee0
okoyorin-cell <okoyorin-cell@users.noreply.github.com>
2026-04-19 23:10:22 +0100

    sync(cursor): 2026-04-19 22:10:22

--- modération/src/events/loggingEvents.js
+++ modération/src/events/loggingEvents.js
@@ -2,6 +2,7 @@ const { Events, AuditLogEvent } = require('discord.js');
 const Logger = require('../modules/logger');
 const CONFIG = require('../config.js');
 const { isProtectedLogChannel, resolveAllLogChannelId } = require('../utils/log-channel-resolve');
+const { shouldSkipMessageDeleteLog } = require('../utils/message-delete-log-skip');
 
 // Stockage pour la sécurité des logs
 const logDeletionHistory = [];
