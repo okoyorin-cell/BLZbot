c463cca662845029ea648c260ccb1465bf0026d2
okoyorin-cell <okoyorin-cell@users.noreply.github.com>
2026-04-24 07:57:07 +0100

    sync(cursor): 2026-04-24 06:57:07

--- reborn-test-bot/src/index.js
+++ reborn-test-bot/src/index.js
@@ -14,6 +14,7 @@ const { refreshApplicationOwners, isOwner } = require('./lib/owners');
 const { registerEarn } = require('./services/earn');
 const { handlePurchase } = require('./services/purchase');
 const { deploySlashCommands } = require('./slashDeploy');
+const { tickSeparations } = require('./services/separation');
 
 cfg.assertToken();
 
